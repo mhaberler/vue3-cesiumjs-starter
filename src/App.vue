a8c63cc7ef3ed09782e5bc3eebd21573aff4b4c1
Michael Haberler <git@mah.priv.at>
2025-07-28 08:44:33 +0200

    vite config

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@ import type { ComponentPublicInstance } from "vue";
 import type { IComponentCesiumMapExpose } from "./types/CesiumMap";
 
 import CesiumMap from "./components/CesiumMap.vue";
-import { waypointsData } from "./data/waypoints";
 
 const cesiumMapRef = ref<ComponentPublicInstance<IComponentCesiumMapExpose> | null>(null);
 const hideMap = ref<boolean>(false);
-const waypointsInterval = ref<ReturnType<typeof setInterval> | undefined>();
 
 const isDark = useDark({
   selector: "html",
@@ -22,44 +20,12 @@ const toggleDark = useToggle(isDark);
 
 const toggleMap = () => (hideMap.value = !hideMap.value);
 
-function initWaypointsDrone() {
-  cesiumMapRef.value?.updateDronePoseAndCamera({
-    id: "705694ff7c7aafb",
-    lon: 2.36145,
-    lat: 48.8592121,
-    alt: 154,
-    gimbal: { yaw: -70, pitch: -10 },
-  });
 
-  cesiumMapRef.value?.addWaypoint({
-    id: "9",
-    lon: 2.4,
-    lat: 48.85,
-    alt: 100,
-    gimbal: { yaw: 90, pitch: -10 },
-  });
-
-  let index = 0;
-  cesiumMapRef.value?.addWaypoint(waypointsData[index], true);
-  index++;
-
-  waypointsInterval.value = setInterval(() => {
-    if (index >= waypointsData.length) {
-      clearInterval(waypointsInterval.value);
-      return;
-    }
-    cesiumMapRef.value?.addWaypoint(waypointsData[index]);
-
-    index++;
-  }, 1000);
-}
 
 onMounted(() => {
-  initWaypointsDrone();
 });
 
 onUnmounted(() => {
-  clearInterval(waypointsInterval.value);
 });
 </script>
 
@@ -68,25 +34,19 @@ onUnmounted(() => {
     <CesiumMap ref="cesiumMapRef" v-show="!hideMap" />
 
     <div class="absolute bottom-2.5 right-2.5 z-50 flex flex-col gap-2 min-w-40">
-      <button
-        v-show="!hideMap"
+      <button v-show="!hideMap"
         class="w-full border p-4 cursor-pointer rounded-md transition text-white bg-sky-900 hover:bg-sky-700"
-        @click="cesiumMapRef?.focusOnWaypointById('5')"
-      >
+        @click="cesiumMapRef?.focusOnWaypointById('5')">
         🔍 Go to Waypoint 5
       </button>
 
-      <button
-        class="w-full border p-4 cursor-pointer rounded-md transition text-white bg-sky-900 hover:bg-sky-700"
-        @click="toggleDark()"
-      >
+      <button class="w-full border p-4 cursor-pointer rounded-md transition text-white bg-sky-900 hover:bg-sky-700"
+        @click="toggleDark()">
         Switch to {{ !isDark ? "🌙" : "☀️" }}
       </button>
 
-      <button
-        class="w-full border p-4 cursor-pointer rounded-md transition text-white bg-sky-900 hover:bg-sky-700"
-        @click="toggleMap()"
-      >
+      <button class="w-full border p-4 cursor-pointer rounded-md transition text-white bg-sky-900 hover:bg-sky-700"
+        @click="toggleMap()">
         {{ !hideMap ? "👁️ Show Map" : "🚫 Hide Map" }}
       </button>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import devtoolsJson from "vite-plugin-devtools-json";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { qrcode } from "vite-plugin-qrcode";
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
 import tailwindcss from "@tailwindcss/vite";
 import cesium from "vite-plugin-cesium";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss(), cesium()],
+const packageJson = JSON.parse(readFileSync("./package.json", "utf-8"));
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const commitMessage = execSync("git log -1  '--pretty=%B'").toString().trim();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+const buildDate = execSync("date -u +%Y-%m-%dT%H:%M:%SZ").toString().trim();
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue(), tailwindcss(), cesium(), qrcode(), devtoolsJson(), vueDevTools()],
+    define: {
+      __GIT_COMMIT_HASH__: JSON.stringify(commitHash),
+      __GIT_COMMIT_MESSAGE__: JSON.stringify(commitMessage),
+      __GIT_BRANCH_NAME__: JSON.stringify(branchName),
+      __VITE_BUILD_DATE__: JSON.stringify(buildDate),
+      __APP_VERSION__: JSON.stringify(packageJson.version),
+    },
+  };
 });
